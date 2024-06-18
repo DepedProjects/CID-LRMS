2c40e79525d6ef18a799d417b2e4bf06f810f99f
Mark Joseph Salem <125126248+MarkSalem804@users.noreply.github.com>
2024-06-18 17:03:36 +0800

    pending calendar

---
+++ src/components/CustomizedCalendar/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+export default function BigCalendar() {
+  return (
+    <div>Calendar</div>
+  )
+}
--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import dayjs from 'dayjs';
 import homeBackground from "../../assets/images/Background-home.png";
+import homeBackground2 from "../../assets/images/about-background.png";
+import homeBackground3 from "../../assets/images/Background-home2.png";
+import homeBackground4 from "../../assets/images/Frame 6.png";
 import homeImage from "../../assets/images/pngegg.png";
-import awardings from "../../assets/images/awards.jpeg.jpg";
+import awardings from "../../assets/images/awards2.jpg";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import Badge from "@mui/material/Badge";
+import { PickersDay } from "@mui/x-date-pickers/PickersDay";
+import CheckIcon from "@mui/icons-material/Check";
 import { GrAchievement } from "react-icons/gr";
-import { Box } from "@mui/material";
+import { TextField, Box, Divider, Typography } from "@mui/material";
 
 export default function Landing() {
+  const [value, setValue] = useState(dayjs(new Date()));
+  const [highlightedDays] = useState([1, 2, 13]);
+
   return (
     <div className="home-container">
       <div className="home-banner-container">
         <div className="home-bannerImage-container">
           <img src={homeBackground} alt="" />
+        </div>
+        <div className="home-bannerImage-container2">
+          <img src={homeBackground2} alt="" />
         </div>
         <div className="home-text-section">
           <h5 style={{ fontFamily: "Fira Sans Condensed", fontSize: "15px" }}>
@@ -48,6 +64,10 @@ export default function Landing() {
           <img src={homeImage} alt="" style={{ maxWidth: "500px" }} />
         </div>
       </div>
+      <div className="home-bannerImage-container3">
+        <img src={homeBackground3} alt="" />
+      </div>
+
       <Box sx={{ mt: 10, textAlign: "center" }}>
         <Box
           sx={{
@@ -81,15 +101,65 @@ export default function Landing() {
       </Box>
       <Box sx={{ display: "flex" }}>
         <Box sx={{ mt: 5 }}>
-          <img src={awardings} alt="" style={{ width: "2300px" }} />
+          <img src={awardings} alt="" style={{ width: "2050px" }} />
         </Box>
-        <Box sx={{ marginTop: 40, ml: -45, width: "700px", color: "white" }}>
-          <h4>
+        <Box sx={{ marginTop: 40, ml: -50, width: "700px", color: "white" }}>
+          <p>
             Photo was taken during the Exhibit of Learning Resources (LRs) last
-            Novermber 25, 2022. All recognition were awarded during the
+            November 25, 2022. All recognition were awarded during the
             CALABARZON Learning Resources Expo at Tagaytay Century Hotel,
             Tagaytay City.
-          </h4>
+          </p>
+        </Box>
+      </Box>
+      <Divider sx={{ marginTop: 10, fontWeight: "bold" }}>
+        <Typography
+          sx={{
+            width: "300px",
+            color: "White",
+            fontSize: "24px",
+            padding: 2,
+            background: "Black",
+            borderRadius: "5px",
+            fontFamily: "Fira Sans Condensed",
+            fontWeight: "bold",
+          }}
+        >
+          SDOIC LRMS Activities
+        </Typography>
+      </Divider>
+      <Box>
+        <img src={homeBackground4} alt="" style={{ marginTop: 20 }} />
+        <Box sx={{ display: "flex", mt: 10 }}>
+          <Box></Box>
+          <Box sx={{ marginLeft: 5, marginTop: -77 }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <StaticDatePicker
+                orientation="portrait"
+                value={value}
+                disableFuture
+                onChange={(newValue) => setValue(newValue)}
+                renderInput={(params) => <TextField {...params} />}
+                renderDay={(day, _value, DayComponentProps) => {
+                  const isSelected =
+                    !DayComponentProps.outsideCurrentMonth &&
+                    highlightedDays.indexOf(day.date()) >= 0;
+
+                  return (
+                    <Badge
+                      key={day.toString()}
+                      overlap="circular"
+                      badgeContent={
+                        isSelected ? <CheckIcon color="red" /> : undefined
+                      }
+                    >
+                      <PickersDay {...DayComponentProps} />
+                    </Badge>
+                  );
+                }}
+              />
+            </LocalizationProvider>
+          </Box>
         </Box>
       </Box>
     </div>
